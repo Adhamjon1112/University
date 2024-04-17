b79a11da27a52789217a6635c0110ebc68a39701
unknown <baxtiyorovadhamjon777@gmail.com>
2024-04-17 15:39:20 +0500

    first commit

---
+++ app_main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+
+def home_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    return render(request, 'app_main/home.html')
+
+
+def teacher_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if not request.user.is_staff and not request.user.is_superuser:
+        return redirect('home')
+    
+
+    return render(request, 'app_main/teachers.html')
